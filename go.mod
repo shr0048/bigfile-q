7f57960cbc9be1cce56342dd34e9ddd664c7d42d
shr0048 <shr0048_dev@protonmail.com>
2021-11-24 17:29:26 +0900

    Update InsertRows

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.17
 
 require (
 	github.com/mattn/go-sqlite3 v1.14.9
-	github.com/shr0048/gocsv v0.0.0-20211123073701-9c224d2721c2
+	github.com/shr0048/gocsv v0.0.0-20211124053845-a254d7d01f30
 )
--- handler/handler.go
+++ handler/handler.go
@@ -2,9 +2,9 @@ package handler
 
 import (
 	"fmt"
-	"reflect"
 	"github.com/shr0048/bigfile-q/driver"
 	"github.com/shr0048/gocsv"
+	"reflect"
 	"strings"
 )
 
@@ -14,6 +14,17 @@ type Handler struct {
 }
 
 func (h *Handler) LoadFileToSQLite() error {
+	// Drop table first, if exist
+	delQuery := "drop table if exists FileDump"
+	statement, _ := h.MySql.SQLClient.Prepare(delQuery)
+	res, err := statement.Exec()
+	if err != nil {
+		fmt.Println("Drop table error")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Table exist, drop first")
+	}
+
 	baseQuery := fmt.Sprintf("create table FileDump (" +
 		"id integer PRIMARY KEY autoincrement, ")
 
@@ -28,8 +39,8 @@ func (h *Handler) LoadFileToSQLite() error {
 	}
 	fmt.Println(baseQuery)
 
-	statement, _ := h.MySql.SQLClient.Prepare(baseQuery)
-	res, err := statement.Exec()
+	statement, _ = h.MySql.SQLClient.Prepare(baseQuery)
+	res, err = statement.Exec()
 	if err != nil {
 		fmt.Println("Create table error")
 		fmt.Println(err)
@@ -46,41 +57,45 @@ func (h *Handler) LoadFileToSQLite() error {
 func (h *Handler) InsertRows() error {
 	var subQuery1 string
 	var subQuery2 string
+	var values []string
 
 	columnNum := h.MyFile.HeaderNum -1
+	for idx, column := range h.MyFile.Header {
+		if idx == columnNum {
+			subQuery1 = subQuery1 + "'" + column + "'"
+		} else {
+			subQuery1 = subQuery1 + "'" + column + "', "
+		}
+	}
 
 	row, ok := h.MyFile.NextRow()
 	for ok {
+		subQuery2 = subQuery2 + "("
 		for idx, column := range row {
 			key := reflect.ValueOf(column).MapKeys()[0].String()
 			if idx == columnNum {
-				subQuery1 = subQuery1 + "'" + key + "'"
 				subQuery2 = subQuery2 + "'" + strings.ReplaceAll(column[key], "'", "''") + "'"
 			} else {
-				subQuery1 = subQuery1 + "'" + key + "', "
 				subQuery2 = subQuery2 + "'" + strings.ReplaceAll(column[key], "'", "''") + "', "
 			}
 		}
+		subQuery2 = subQuery2 + ")"
+		values = append(values, subQuery2)
 
-		query := fmt.Sprintf("INSERT INTO 'FileDump' (%s) VALUES (%s)", subQuery1, subQuery2)
-		fmt.Println(query)
-
-		statement, _ := h.MySql.SQLClient.Prepare(query)
-		res, err := statement.Exec()
-		if err != nil {
-			fmt.Println("Create table error")
-			fmt.Println(err)
-
-			return err
-		} else {
-			fmt.Println(res.LastInsertId())
-			fmt.Println(res.RowsAffected())
-		}
-
-		subQuery1 = ""
 		subQuery2 = ""
-
 		row, ok =  h.MyFile.NextRow()
+	}
+
+	query := fmt.Sprintf("INSERT INTO 'FileDump' (%s) VALUES %s", subQuery1, strings.Join(values, ","))
+	fmt.Println(query)
+
+	statement, _ := h.MySql.SQLClient.Prepare(query)
+	_, err := statement.Exec()
+	if err != nil {
+		fmt.Println("Data insert error")
+		fmt.Println(err)
+
+		return err
 	}
 
 	return nil
